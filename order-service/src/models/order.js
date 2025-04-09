a2e5437acc5e9cad1a2df17e0b8d3791c143be55
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2025-04-09 23:57:13 +0700

    update

--- order-service/src/models/order.js
+++ order-service/src/models/order.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
     orderItems: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: "Product",
         required: true,
       },
     ],
